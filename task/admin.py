f8aba1196c42c1496351257e4ef28638fa50c006
amirhossein zinati <amirhosseinzt78@gmail.com>
2024-06-20 10:21:47 +0330

    admin

--- task/admin.py
+++ task/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from task.models import Category, Task, SubTask, Reminder, Attachment
+from task.models import Category, Task, SubTask, Reminder, Attachment, Participation
 
 admin.site.register(Category)
 admin.site.register(Task)
 admin.site.register(SubTask)
 admin.site.register(Reminder)
 admin.site.register(Attachment)
+admin.site.register(Participation)
